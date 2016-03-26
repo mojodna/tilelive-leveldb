b511e749594a07ae3b07bf255c879ddabe43d868
Seth Fitzsimmons <seth@mojodna.net>
2016-03-25 21:46:38 -0500

    Complete initial implementation
    Drops streaming implementation as it's since been done in
    tilelive-streaming.
    
    Purpose-build createReadStream and createWriteStream methods would still
    be beneficial, however.

--- index.js
+++ index.js
@@ -2,22 +2,27 @@
 
 var crypto = require("crypto"),
     path = require("path"),
-    stream = require("stream"),
     url = require("url");
 
 var async = require("async"),
     level = require("level");
+
+// TODO store db instances outside of individual objects (in a locking cache) so
+// that multiple LevelDB instances can be open simultaneously providing
+// different formats and scales
 
 var LevelDB = function(uri, callback) {
   if (typeof(uri) === "string") {
     uri = url.parse(uri, true);
   }
 
+  uri.query = uri.query || {};
+
   this.uri = uri;
   this.path = path.join(uri.hostname, uri.pathname);
-
-  // TODO scale (to support multi-scale archives)
-  // TODO format (to support multi-format archives)
+  // support for multi-scale/multi-format archives
+  this.scale = uri.query.scale || 1;
+  this.format = uri.query.format || "";
 
   return this.open(function(err) {
     if (err) {
@@ -28,119 +33,59 @@ var LevelDB = function(uri, callback) {
   }.bind(this));
 };
 
-LevelDB.prototype.createReadStream = function(options) {
-  // options semantics borrowed from LevelUP
-  // options.start
-  // options.end
-  // options.bbox
-  // options.* - options that get passed along with coordinates to getTile()
-  options.keys = "keys" in options ? options.keys : true;
-  options.values = "values" in options ? options.values : true;
-  options.limit = "limit" in options ? options.limit : -1;
-
-  throw new Error("Not implemented: createReadStream()");
-};
-
-LevelDB.prototype.createKeyStream = function(options) {
-  options.keys = true;
-  options.values = false;
-
-  return this.createReadStream(options);
-};
-
-LevelDB.prototype.createWriteStream = function() {
-  throw new Error("Not implemented: createWriteStream()");
-};
-
 LevelDB.prototype.getTile = function(zoom, x, y, callback) {
   callback = callback || function() {};
   zoom = zoom | 0;
   x = x | 0;
   y = y | 0;
 
-  var tileStream = new stream.PassThrough(),
-      dests = [],
-      _pipe = tileStream.pipe,
-      db = this.db;
-
-  tileStream.pipe = function(dest) {
-    dests.push(dest);
-
-    return _pipe.apply(this, arguments);
-  };
-
-  tileStream.zoom = zoom;
-  tileStream.x = x;
-  tileStream.y = y;
-
-  if (!db) {
-    var err = new Error("Archive doesn't exist: " + url.format(this.uri));
-    tileStream.emit("error", err);
-    tileStream.end();
-
-    return callback(err);
+  if (!this.db) {
+    return callback(new Error("Archive doesn't exist: " + url.format(this.uri)));
   }
 
-  var coords = [zoom, x, y].join("/"),
-      // TODO incorporate all options into the key (when options replace (z,x,y))
-      key = "coords:" + coords;
+  // TODO incorporate all options into the key (when this.options replace (z,x,y))
+  var key = [zoom, x, y].join("/");
 
-  db.get(key, {
-    valueEncoding: "utf8"
-  }, function(err, md5) {
+  return async.parallel({
+    headers: async.apply(this.db.get.bind(this.db), "headers:" + key, {
+      valueEncoding: "json"
+    }),
+    data: async.apply(this.db.get.bind(this.db), "data:" + key),
+    md5: async.apply(this.db.get.bind(this.db), "md5:" + key, {
+      valueEncoding: "utf8"
+    })
+  }, function(err, results) {
     if (err) {
-      tileStream.emit("error", err);
-      tileStream.end();
-
       return callback(err);
     }
 
-    return async.parallel({
-      headers: async.apply(db.get.bind(db),
-                           "headers:" + coords,
-                           {
-                             valueEncoding: "json"
-                           }),
-      data: async.apply(db.get.bind(db),
-                        "data:" + md5)
-    }, function(err, results) {
-      if (err) {
-        tileStream.emit("error", err);
-        tileStream.end();
+    var headers = results.headers,
+        data = results.data,
+        md5 = results.md5,
+        hash = crypto.createHash("md5").update(data).digest().toString("hex");
 
-        return callback(err);
-      }
+    if (md5 !== hash) {
+      return callback(new Error(util.format("MD5 values don't match for %s", key)));
+    }
 
-      var headers = results.headers,
-          data = results.data;
+    // TODO if data == null, pass an Exception
 
-      headers["content-md5"] = md5;
-
-      dests.forEach(function(dest) {
-        // http.ServerResponse-compatible piping
-        if (dest.setHeader) {
-          Object.keys(headers).forEach(function(x) {
-            dest.setHeader(x, headers[x]);
-          });
-        }
-      });
-
-      tileStream.emit("headers", headers);
-      tileStream.end(data);
-
-      return callback(null, data, headers);
-    });
+    return callback(null, data, headers);
   });
-
-  return tileStream;
 };
 
-LevelDB.prototype._putTile = function(zoom, x, y, data, headers, callback) {
+LevelDB.prototype.putTile = function(zoom, x, y, data, headers, callback) {
+  if (typeof(headers) === "function") {
+    callback = headers;
+    headers = {};
+  }
+
   headers = headers || {};
   callback = callback || function() {};
   zoom = zoom | 0;
   x = x | 0;
   y = y | 0;
+  var self = this;
 
   // normalize header names
   var _headers = {};
@@ -153,132 +98,127 @@ LevelDB.prototype._putTile = function(zoom, x, y, data, headers, callback) {
   if (_headers["content-md5"]) {
     md5 = _headers["content-md5"];
   } else {
-    var hash = crypto.createHash("md5");
+    md5 = crypto.createHash("md5").update(data).digest().toString("hex");
 
-    hash.update(data);
-
-    md5 = hash.digest().toString("hex");
+    _headers["content-md5"] = md5;
   }
 
-  // this is a redundant header, as it's present in the key
-  delete _headers["content-md5"];
-
-  var coords = [zoom, x, y].join("/"),
-      // TODO incorporate all supported options in the key if options were
-      // provided
-      coordinatesKey = "coords:" + coords,
-      headersKey = "headers:" + coords,
-      dataKey = "data:" + md5;
+  // TODO incorporate all supported options in the key if options were
+  // provided
+  var key = [zoom, x, y].join("/");
 
   return this.openForWrite(function(err) {
     if (err) {
       return callback(err);
     }
 
-    var db = this.db;
-
-    return db.get(dataKey, function(err, value) {
-      if (err && err.type !== "NotFoundError") {
-        return callback(err);
+    var operations = [
+      {
+        type: "put",
+        key: "md5:" + key,
+        value: md5,
+        valueEncoding: "utf8"
+      },
+      {
+        type: "put",
+        key: "headers:" + key,
+        value: headers,
+        valueEncoding: "json"
+      },
+      {
+        type: "put",
+        key: "data:" + key,
+        value: data
       }
+    ];
 
-      var operations = [
-        {
-          type: "put",
-          key: coordinatesKey,
-          value: md5,
-          valueEncoding: "utf8"
-        },
-        {
-          type: "put",
-          key: headersKey,
-          value: headers,
-          valueEncoding: "json"
-        }
-      ];
-
-      if (!value) {
-        // only write data if it wasn't already there
-        operations.push({
-          type: "put",
-          key: dataKey,
-          value: data
-        });
-      }
-
-      return db.batch(operations, callback);
-    });
-  }.bind(this));
+    return self.db.batch(operations, callback);
+  });
 };
 
-LevelDB.prototype.putTile = function(zoom, x, y) {
-  // outputs: leveldb.getTile(0, 0, 0).pipe(leveldb.putTile())
-  // TODO make headers an optional argument
-
-  if (arguments.length > 0) {
-    // callback style
-    return this._putTile.apply(this, arguments);
+LevelDB.prototype.getInfo = function(callback) {
+  if (!this.db) {
+    return callback(new Error("Archive doesn't exist: " + url.format(this.uri)));
   }
 
-  var tileStream = new stream.Transform(),
-      chunks = [],
-      sink = this;
+  return this.db.get("info", {
+    valueEncoding: "json"
+  }, callback);
+};
 
-  tileStream
-    .on("pipe", function(src) {
-      // use strict checks to allow 0 values
+LevelDB.prototype.putInfo = function(info, callback) {
+  callback = callback || function() {};
+  var self = this;
 
-      this.zoom = src.zoom === undefined ? zoom : src.zoom;
-      this.x = src.x === undefined ? x : src.x;
-      this.y = src.y === undefined ? y : src.y;
-      this.headers = this.headers || {};
-    })
-    .on("headers", function(headers) {
-      this.headers = headers;
-    });
+  return this.openForWrite(function(err) {
+    if (err) {
+      return callback(err);
+    }
 
-  tileStream._transform = function(chunk, encoding, callback) {
-    chunks.push(chunk);
-
-    return callback();
-  };
-
-  tileStream._flush = function(callback) {
-    return sink._putTile(this.zoom, this.x, this.y, Buffer.concat(chunks), this.headers, callback);
-  };
-
-  tileStream.setHeader = function(name, value) {
-    this.headers[name] = value;
-  };
-
-  return tileStream;
+    return self.db.put("info", info, {
+      valueEncoding: "json"
+    }, callback);
+  });
 };
 
 LevelDB.prototype.dropTile = function(zoom, x, y, callback) {
   callback = callback || function() {};
+  zoom = zoom | 0;
+  x = x | 0;
+  y = y | 0;
+
+  var key = [zoom, x, y].join("/"),
+      db = this.db;
+
+  return this.openForWrite(function(err) {
+    if (err) {
+      return callback(err);
+    }
+
+    var operations = [
+      {
+        type: "del",
+        key: "md5:" + key
+      },
+      {
+        type: "del",
+        key: "headers:" + key
+      },
+      {
+        type: "del",
+        key: "data:" + key
+      }
+    ];
+
+    return db.batch(operations, callback);
+  });
 };
 
 LevelDB.prototype.open = function(callback) {
   callback = callback || function() {};
+  var self = this;
 
   this.db = level(this.path, {
+    // TODO play with blockSize
+    // TODO extract options
     createIfMissing: false,
     valueEncoding: "binary"
   }, function(err) {
     if (err) {
       if (err.type === "OpenError") {
-        this.db = null;
+        self.db = null;
       } else {
         return callback(err);
       }
     }
 
     return callback();
-  }.bind(this));
+  });
 };
 
 LevelDB.prototype.openForWrite = function(callback) {
   callback = callback || function() {};
+  var self = this;
 
   var close = function(cb) {
     return cb();
@@ -294,23 +234,29 @@ LevelDB.prototype.openForWrite = function(callback) {
       return callback(err);
     }
 
-    this.db = level(this.path, {
+    return level(self.path, {
+      // TODO play with blockSize
       createIfMissing: true,
       valueEncoding: "binary"
-    }, callback);
-  }.bind(this));
+    }, function(err, db) {
+      if (err) {
+        return callback(err);
+      }
+
+      self.db = db;
+
+      return callback();
+    });
+  });
 };
 
-// TODO openForBulkWrite
+// TODO openForBulkWrite w/ tuned options (writeBufferSize)
 
 LevelDB.prototype.close = function(callback) {
   callback = callback || function() {};
 
   return this.db.close(callback);
 };
-
-// blob:<MD5>
-// <key>:<MD5>
 
 LevelDB.registerProtocols = function(tilelive) {
   tilelive.protocols["leveldb+file:"] = this;
--- test.js
+++ test.js
@@ -1,29 +1,48 @@
+"use strict";
+
+var async = require("async");
+
 var LevelDB = require("./")({ protocols: {} });
 
 new LevelDB("leveldb+file://./tiles", function(err, src) {
   // TODO assert that the tiles directory doesn't exist yet
-  // return src.putTile(0, 0, 0, new Buffer("foo"), {}, function(err) {
-  //   if (err) {
-  //     console.error(err.stack);
-  //     throw err;
-  //   }
+  async.series([
+    function(done) {
+      return src.putTile(0, 0, 0, new Buffer("foo"), function(err) {
+        if (err) {
+          console.error(err.stack);
+          throw err;
+        }
 
-  //   // TODO assert that the tiles directory was created
+        // TODO assert that the tiles directory was created
 
-  //   console.log("Tile written");
-  // });
+        console.log("Tile written");
 
-  // return src.getTile(0, 0, 0, function(err, data, headers) {
-  //   console.log(arguments);
-  // });
+        return done();
+      });
+    },
+    function(done) {
+      return src.getTile(0, 0, 0, function(err, data, headers) {
+        console.log("getTile:", arguments);
 
-  src.getTile(0, 0, 0)
-    .on("headers", function() {
-      console.log("headers:", arguments);
-    })
-    .on("data", function(chunk) {
-      console.log(arguments);
-    }).pipe(process.stdout);
+        return done();
+      });
+    },
+    function(done) {
+      return src.putInfo({
+        scheme: "xyz",
+        minzoom: 10,
+        maxzoom: 22
+      }, done);
+    },
+    function(done) {
+      return src.getInfo(function(err, info) {
+        console.log("getInfo:", arguments);
+
+        return done();
+      });
+    }
+  ], function(err, data) {
+    // console.log(arguments);
+  });
 });
-
-
